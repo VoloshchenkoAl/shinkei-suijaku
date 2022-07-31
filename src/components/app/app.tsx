52ef354f5f23e542b961fc570e65394b9768f9eb
avo <voloshchenkoal@gmail.com>
2022-08-01 00:23:47 +0300

    feat: :art: add new layout for game

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,11 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 /* @Pages */
 import { WelcomePage } from 'pages/welcome-page';
+import { PreparePage } from 'pages/prepare-page';
+
+/* @Components */
+import { Layout } from 'components/layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<WelcomePage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="prepare" element={<PreparePage />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/pages/prepare-page/index.ts
@@ -0,0 +1,1 @@
+export { default as PreparePage } from './prepare-page';
